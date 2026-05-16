d0a53963dfe165915ed7369091e0e736f139bda2
Prasanth Ramaswamy <prasanthramaswamy01@gmail.com>
2026-05-16 16:00:55 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Addition:", add(2, 3))
+    print("Multiplication:", multiply(2, 3))
